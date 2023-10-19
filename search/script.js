865fd42798f605cd5a74bf2e510e6feb9d8bc89c
SSA Tools <148066903+ssatools@users.noreply.github.com>
2023-10-19 22:17:28 +0545

    Update script.js

--- search/script.js
+++ search/script.js
@@ -1,77 +1,75 @@
-$(document).ready(function () {
-    var Shuffle = window.shuffle;
+        $(document).ready(function () {
+            var Shuffle = window.shuffle;
 
-    var liData = [
-        {
-            name: "Arrow1",
-            img: "arrow.png",
-            link: "https://example.com/arrow",
-        },
-        {
-            name: "Arrow2",
-            img: "arrow.png",
-            link: "https://example.com/arrow",
-        },
-    ];
+            var liData = [
+                {
+                    name: "Arrow1",
+                    img: "arrow.png",
+                    link: "https://example.com/arrow",
+                },
+                {
+                    name: "Arrow2",
+                    img: "arrow.png",
+                    link: "https://example.com/arrow",
+                },
+            ];
 
-    var liList = document.getElementById("sgrid");
+            var liList = document.getElementById("sgrid");
 
-    liData.forEach(function (item) {
-        var listItem = document.createElement("li");
-        listItem.className = "icons";
+            liData.forEach(function (item) {
+                var listItem = document.createElement("li");
+                listItem.className = "icons";
+                listItem.setAttribute("data-item-name", item.name); // Add data attribute
 
-        // Create an image element and set the source
-        var image = document.createElement("img");
-        image.src = item.img;
-        listItem.appendChild(image);
+                var image = document.createElement("img");
+                image.src = item.img;
+                listItem.appendChild(image);
 
-        // Create a span element for the text
-        var textSpan = document.createElement("span");
-        textSpan.textContent = item.name;
-        listItem.appendChild(textSpan);
+                var textSpan = document.createElement("span");
+                textSpan.textContent = item.name;
+                listItem.appendChild(textSpan);
 
-        listItem.addEventListener("click", function () {
-            // Open the specified link when the item is clicked
-            window.location.href = item.link;
+                listItem.addEventListener("click", function () {
+                    window.location.href = item.link;
+                });
+
+                liList.appendChild(listItem);
+            });
+
+            var bookList = function (element) {
+                this.element = element;
+                this.shuffle = new Shuffle(element, {
+                    itemSelector: ".ssatoolshome-list-wrap ul>li", // Adjust the item selector
+                });
+
+                this.addSearchFilter();
+            };
+
+            bookList.prototype.addSearchFilter = function () {
+                var searchInput = $("#shuffle-search")[0];
+
+                if (!searchInput) {
+                    return;
+                }
+
+                searchInput.addEventListener("keyup", this._handleSearchKeyup.bind(this));
+            };
+
+            bookList.prototype._handleSearchKeyup = function (evt) {
+                var searchInput = $("#shuffle-search")[0];
+                var searchText = evt.target.value.toLowerCase();
+
+                if (searchInput && searchInput.value) {
+                    $(".catalog-search").addClass("input--filled");
+                } else {
+                    $(".catalog-search").removeClass("input--filled");
+                }
+
+                this.shuffle.filter(function (element, shuffle) {
+                    var itemName = element.getAttribute("data-item-name");
+                    return itemName.toLowerCase().indexOf(searchText) !== -1;
+                });
+            };
+
+            window.book_list = new bookList($(".ssatoolshome-list-wrap > ul"));
         });
-
-        liList.appendChild(listItem);
-    });
-
-    var bookList = function (element) {
-        this.element = element;
-        this.shuffle = new Shuffle(element, {
-            itemSelector: ".ssatoolshome-list > li",
-        });
-
-        this.addSearchFilter();
-    };
-
-    bookList.prototype.addSearchFilter = function () {
-        var searchInput = $("#shuffle-search")[0];
-
-        if (!searchInput) {
-            return;
-        }
-
-        searchInput.addEventListener("keyup", this._handleSearchKeyup.bind(this));
-    };
-
-    bookList.prototype._handleSearchKeyup = function (evt) {
-        var searchInput = $("#shuffle-search")[0];
-        var searchText = evt.target.value.toLowerCase();
-
-        if (searchInput && searchInput.value) {
-            $(".catalog-search").addClass("input--filled");
-        } else {
-            $(".catalog-search").removeClass("input--filled");
-        }
-
-        this.shuffle.filter(function (element, shuffle) {
-            var itemName = element.getAttribute("data-item-name");
-            return itemName.toLowerCase().indexOf(searchText) !== -1;
-        });
-    };
-
-    window.book_list = new bookList($(".ssatoolshome-list-wrap > ul"));
-});
