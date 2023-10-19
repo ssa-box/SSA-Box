db32b8370f3a4aa59ee5dbc43a264d29656ab416
SSA Tools <148066903+ssatools@users.noreply.github.com>
2023-10-19 21:09:02 +0545

    Update script.js

--- search/script.js
+++ search/script.js
@@ -1,30 +1,29 @@
-    $("ul li").each(function (i) {
-        var itemName = $(this).text().toLowerCase().trim();
-        $(this).attr("data-item-name", itemName);
-    })
+$(document).ready(function () {
     var Shuffle = window.shuffle;
+
     var bookList = function (element) {
         this.element = element;
-
         this.shuffle = new Shuffle(element, {
-            itemSelector: ".ssatoolshome-list-wrap > ul > li"
+            itemSelector: ".ssatoolshome-list-wrap ul > li",
         });
-        this._activeFilters = [];
+
         this.addSearchFilter();
     };
-    bookList.prototype.toArray = function (arrayLike) {
-        return Array.prototype.slice.call(arrayLike);
-    };
+
     bookList.prototype.addSearchFilter = function () {
         var searchInput = $("#shuffle-search")[0];
+
         if (!searchInput) {
             return;
         }
+
         searchInput.addEventListener("keyup", this._handleSearchKeyup.bind(this));
     };
+
     bookList.prototype._handleSearchKeyup = function (evt) {
         var searchInput = $("#shuffle-search")[0];
         var searchText = evt.target.value.toLowerCase();
+
         if (searchInput && searchInput.value) {
             $(".catalog-search").addClass("input--filled");
         } else {
@@ -32,11 +31,10 @@
         }
 
         this.shuffle.filter(function (element, shuffle) {
-            var titleElement = element.querySelector(".ssatoolshome-list-wrap ul li a");
-            var titleText = titleElement.textContent.toLowerCase().trim();
-            return titleText.indexOf(searchText) !== -1;
+            var itemName = element.getAttribute("data-item-name");
+            return itemName.indexOf(searchText) !== -1;
         });
     };
-    $(document).ready(function () {
-        window.book_list = new bookList($(".ssatoolshome-list-wrap > ul"));
-    })
+
+    window.book_list = new bookList($(".ssatoolshome-list-wrap > ul"));
+});
