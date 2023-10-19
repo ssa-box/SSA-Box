b51c2d97635fe091834e4ce7b05c6670f715e616
SSA Tools <148066903+ssatools@users.noreply.github.com>
2023-10-19 22:57:15 +0545

    Update script.js

--- search/script.js
+++ search/script.js
@@ -4,6 +4,66 @@
             var liData = [
                 {
                     name: "Tool1",
+                    img: "./ssatoolimg.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool2",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool3",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool4",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool5",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool6",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool7",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool8",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool9",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool10",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool11",
+                    img: "arrow.png",
+                    link: "#",
+                },
+                {
+                    name: "Tool1",
                     img: "arrow.png",
                     link: "#",
                 },
